33e2db898d7a765911a9adb9f58a272d44660d49
tom <782454854@qq.com>
2019-06-05 13:14:09 +0800

    删除信息

--- my_time/src/main/java/cn/tomiaocat/pojo/repository/TimerUrlRepository.java
+++
@@ -1,8 +0,0 @@
-package cn.tomiaocat.pojo.repository;
-
-import cn.tomiaocat.pojo.TimerUrl;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TimerUrlRepository extends JpaRepository<TimerUrl,Long> {
-
-}
--- my_time/src/main/java/cn/tomiaocat/pojo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package cn.tomiaocat.pojo.repository;
-
-import cn.tomiaocat.pojo.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
